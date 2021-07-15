27f7c0c29057c04639b17093d44336c894b68270
Jihun Yu <30788586+wlgns12370@users.noreply.github.com>
2021-07-15 14:46:45 +0900

    modify top_posts_crawling.py for more

--- top_posts_crawling.py
+++ top_posts_crawling.py
@@ -2,23 +2,23 @@ import dload
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+
 driver = webdriver.Chrome('chromedriver')
-
-
 driver.get("https://www.instagram.com/explore/tags/ootd/")
-time.sleep(3)
+time.sleep(5)
 
 req = driver.page_source
 soup = BeautifulSoup(req,'html.parser')
-
-thumbnails = soup.select('#react-root > section > main > article > div.EZdmt > div > div > div:nth-child(1) > div:nth-child(1) > a > div > div.KL4Bh > img')
-
-i = 1
-for thumbnail in thumbnails:
-    img = thumbnail['src']
-    dload.save(img, f'img/{i}.jpg')
-    i += 1
-
+c =1
+for i in range(1,4):
+    for j in range(1,4):
+        # html of the tag you want
+        thumbnails = soup.select(f'#react-root > section > main > article > div.EZdmt > div > div > div:nth-child({i}) > div:nth-child({j}) > a > div > div.KL4Bh > img')
+        for thumbnail in thumbnails:
+            img = thumbnail['src']
+            print(img)
+            dload.save(img, f'img/{c}.jpg')
+            c += 1
 
 driver.quit()
 
